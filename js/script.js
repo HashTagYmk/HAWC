030f76efa5ed568d09f2acb425711de423c68df9
Mwape Yumba Mbuzi <61735331+HashTagYmk@users.noreply.github.com>
2026-07-01 16:13:00 +0200

    update 1.2
    updated with new vidoes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.body.addEventListener("touchstart", playVideo);
-    function playVideo() {
-        const video = document.getElementById('myVideo');
-        if(video.playing) {
-        } else {
-            video.play();
-        }
+const video = document.getElementById("myVideo");
+
+const totalVideos = 6;
+
+let currentVideo = 1;
+
+function playVideo() {
+    video.src = `videos/HAWC${currentVideo}.mp4`;
+
+    video.load();
+
+    video.play().catch(error => {
+        console.log("Autoplay prevented:", error);
+    });
+}
+
+playVideo();
+
+video.addEventListener("ended", () => {
+
+    currentVideo++;
+    
+    if (currentVideo > totalVideos) {
+        currentVideo = 1;
     }
+
+    playVideo();
+
 });
